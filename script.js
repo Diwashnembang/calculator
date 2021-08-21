bd39dbd9212fdc773d1394368c1cd3cc345b6ce8
Diwash Nembang <nembangd0@gmail.com>
2021-08-21 22:12:02 +0545

    added working calculator

--- script.js
+++ script.js
@@ -1,49 +1,93 @@
-function getInput(){
-    const inputsNode=document.querySelectorAll("#body");
-    const display=document.querySelector("#display");
+function appendNumbers(number){
+    if(currentNumber.includes(".") && number===".") return;
+    currentNumber= currentNumber+number;
 
 
-    const inputs=Array.from(inputsNode);
-    let firstNumber , secondNumber;
-    inputs.forEach(input=>{
-        input.addEventListener("click",(e)=>{
-            let number=e.target.getAttribute("data-number")
+}
 
-            console.log(number)
-            if(number!=="+" && number!=="="){
 
-                 firstNumber=display.textContent;
-                 secondNumber=e.target.textContent;
-                display.textContent=e.target.textContent;
-                console.log(firstNumber,secondNumber)
+function chooseOperation(operation){
+    if(currentNumber==="") return;
+    if(previousNumber!==""){
+        compute()
+    }
+    whatTodo=operation;
+    previousNumber=currentNumber;
+    currentNumber='';
+    // whatTodo=undefined;
+    
+    
+}
+
+
+function compute(){
+    switch (whatTodo){
+        case '+':
+            currentNumber=+currentNumber+ +previousNumber;
+            break;
+        case '-':
+            currentNumber=+previousNumber - currentNumber;
+            break;
+        case '*':
+             currentNumber=+currentNumber * +previousNumber;
+            break;  
+        
+        case '/':
+            currentNumber=+currentNumber / +previousNumber;
+            break; 
+
+        default:
+            return;
             }
-                if(number==="="){
-                    display.textContent= +firstNumber+ +secondNumber;
-                    console.log("in")
-                }
-            }
 
-        )
+        whatTodo=undefined;
+        previousNumber=" ";
+        
+        
+    }
+    
+    
+function updateScreen(){
+        currentElement.textContent=currentNumber;
+
+            previousElement.textContent=previousNumber
+    }
+
+const numberButtons=document.querySelectorAll("[data-number]");
+const operationButton=document.querySelectorAll("[data-operation]");
+const clearButton=document.querySelector("[data-clear]");
+const deletButton=document.querySelector("[data-delet");
+const equallButton=document.querySelector("[data-equall]");
+
+let currentElement=document.querySelector(".currentDisplay");
+let  previousElement=document.querySelector(".previousDisplay");
+let currentNumber="";
+let previousNumber="";
+let whatTodo;
+
+console.log(operationButton)
+
+
+numberButtons.forEach(button=>{
+    button.addEventListener("click",()=>{
+        appendNumbers(button.textContent);
+        updateScreen();
+        
     })
+});
 
-}
+operationButton.forEach(button=>{
+    button.addEventListener("click",e=>{
+        chooseOperation(button.textContent);
+        updateScreen()
+    })
+})
+    
+equallButton.addEventListener("click",()=>{
+    compute();
+    updateScreen();
+
+})
 
 
 
-
-
-
-
-
-
-
-
-
-
-function main(){
-    let c=getInput();
-   
-
-}
-
-main()
