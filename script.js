c2c86a213838617c79574fedba265e87da674b69
Diwash Nembang <nembangd0@gmail.com>
2021-08-22 21:01:23 +0545

    fixed divide

--- script.js
+++ script.js
@@ -1,120 +1,102 @@
-
-
-
-function clear(){
-    currentNumber='';
-    previousNumber='';
-    whatTodo='';
+function clear() {
+    currentNumber = "";
+    previousNumber = "";
+    whatTodo = "";
 }
 
-
-function delet(){
-    let newString=Array.from(currentNumber);
-    newString.splice(newString.length-1,1);
-    currentNumber=newString.join("");
-
+function delet() {
+    let newString = Array.from(currentNumber);
+    newString.splice(newString.length - 1, 1);
+    currentNumber = newString.join("");
 }
 
-function appendNumbers(number){
-    if(currentNumber.includes(".") && number===".") return;
-    currentNumber= currentNumber+number;
-
-
+function appendNumbers(number) {
+    if (currentNumber.includes(".") && number === ".") return;
+    currentNumber = currentNumber + number;
 }
 
-
-function chooseOperation(operation){
-    if(currentNumber==="") return;
-    if(previousNumber!==""){
-        compute()
+function chooseOperation(operation) {
+    if (currentNumber === "") return;
+    if (previousNumber !== "") {
+        compute();
     }
-    whatTodo=operation;
-    previousNumber=currentNumber;
-    currentNumber='';
+    whatTodo = operation;
+    previousNumber = currentNumber;
+    currentNumber = "";
     // whatTodo=undefined;
-    
-    
 }
 
-
-function compute(){
-    switch (whatTodo){
-        case '+':
-            currentNumber=+currentNumber+ +previousNumber;
+function compute() {
+    switch (whatTodo) {
+        case "+":
+            currentNumber = +currentNumber + +previousNumber;
             break;
-            case '-':
-                currentNumber=+previousNumber - currentNumber;
-                break;
-                case '*':
-                    currentNumber=+currentNumber * +previousNumber;
-                    break;  
-                    
-                    case '/':
-                        currentNumber=+currentNumber / +previousNumber;
-                        break; 
-                        
-                        default:
-                            return;
-                        }
-                        
-                        console.log(typeof(currentNumber))
-                        whatTodo=undefined;
-                        previousNumber=" ";
-                        console.log("the",currentNumber)
-        
-    }
-    
-    
-function updateScreen(){
-        currentElement.textContent=currentNumber;
+        case "-":
+            currentNumber = +previousNumber - currentNumber;
+            break;
+        case "*":
+            currentNumber = +currentNumber * +previousNumber;
+            break;
 
-            previousElement.textContent=previousNumber
+        case "/":
+            currentNumber = +previousNumber / +currentNumber;
+            break;
+
+        default:
+            return;
     }
 
-const numberButtons=document.querySelectorAll("[data-number]");
-const operationButton=document.querySelectorAll("[data-operation]");
-const clearButton=document.querySelector("[data-clear]");
-const deletButton=document.querySelector("[data-delet");
-const equallButton=document.querySelector("[data-equall]");
+    console.log(typeof currentNumber);
+    whatTodo = undefined;
+    previousNumber = " ";
+    console.log("the", currentNumber);
+}
 
-let currentElement=document.querySelector(".currentDisplay");
-let  previousElement=document.querySelector(".previousDisplay");
-let currentNumber="";
-let previousNumber="";
+function updateScreen() {
+    currentElement.textContent = currentNumber;
+
+    previousElement.textContent = previousNumber;
+}
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButton = document.querySelectorAll("[data-operation]");
+const clearButton = document.querySelector("[data-clear]");
+const deletButton = document.querySelector("[data-delet");
+const equallButton = document.querySelector("[data-equall]");
+
+let currentElement = document.querySelector(".currentDisplay");
+let previousElement = document.querySelector(".previousDisplay");
+let currentNumber = "";
+let previousNumber = "";
 let whatTodo;
 
-console.log(operationButton)
+console.log(operationButton);
 
-
-numberButtons.forEach(button=>{
-    button.addEventListener("click",()=>{
+numberButtons.forEach((button) => {
+    button.addEventListener("click", () => {
         appendNumbers(button.textContent);
         updateScreen();
-        
-    })
+    });
 });
 
-operationButton.forEach(button=>{
-    button.addEventListener("click",e=>{
+operationButton.forEach((button) => {
+    button.addEventListener("click", (e) => {
         chooseOperation(button.textContent);
-        updateScreen()
-    })
-})
-    
-equallButton.addEventListener("click",()=>{
+        updateScreen();
+    });
+});
+
+equallButton.addEventListener("click", () => {
     compute();
     updateScreen();
+});
 
-})
-
-clearButton.addEventListener("click",()=>{
+clearButton.addEventListener("click", () => {
     clear();
     updateScreen();
-})
+});
 
-
-deletButton.addEventListener("click",()=>{
+deletButton.addEventListener("click", () => {
     delet();
     updateScreen();
-})
-
+});
